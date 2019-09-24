90d34500cac275e9e93883afce2a8773095641fe
luanaiglesias <luanaeiglesias@gmail.com>
2019-09-24 13:00:04 -0300

    FAQ

--- FAQ.php
+++ FAQ.php
@@ -67,25 +67,100 @@
 
     </header>
 
-  <main>
-    <div class="row">
-    <div class="col-lg-6 col-xl-6 col-md-6 mx-auto conocenos">
-      <h1>Preguntas frecuentes</h1>
-      <h3>¿Quiénes somos?</h3>
-      <p>Somos el espacio donde todas las actividades sociales convergen. Si te gustaria participar de algun voluntariado, si sos el titular de alguna organización o empresa social, este lugar te ayudará a potenciar tu trabajo!</p>
-      <h3>¿Cómo puedo contactarlos?</h3>
-      <p>Podes contactarnos a través del
-        <a href="contactanos.php">Formulario de contacto</a> o enviando tu inquietudes a sharityx@gmail.com</p>
-      <h3>¿Cómo puedo participar?</h3>
-      <p>Para poder participar debes estar registrado, ya seas una persona o una organización.</p>
-      <h3>¿Tiene algún costo?</h3>
-      <p>No! Desde Sharityx deseamos que seamos muchos buscando transformar la realidad, es por ellos que este espacio será siempre gratuito.</p>
-      <h3>¿Cómo cambio la info de mi perfil?</h3>
-      <p>Podes cambiar la información de tu perfil a través de la pestaña "Editar" en la solapa <a href="perfil.php">Perfil</a> </p>
+    <main>
+      <div class="row">
+      <div class="col-lg-6 col-xl-6 col-md-6 mx-auto conocenos">
 
-      <h3>¿Cómo cambio mi contraseña?</h3>
-      <p>Podes cambiar tu contraseña a través de la pestaña "Cambiar contraseña" en la solapa "Seguridad" </p>
-  </main>
+        <div class="accordion" id="accordionExample">
+  <h1>Preguntas frecuentes</h1>
+    <div class="card">
+      <div class="card-header" id="headingOne">
+        <h2 class="mb-0">
+
+          <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            ¿Quiénes somos?
+          </button>
+        </h2>
+      </div>
+
+      <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+        <div class="card-body">
+          Somos el espacio donde todas las actividades sociales convergen. Si te gustaria participar de algun voluntariado o si sos el titular de alguna organización o empresa social, este lugar te ayudará a potenciar tu trabajo!
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header" id="headingTwo">
+        <h2 class="mb-0">
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            ¿Cómo puedo contactarlos?
+          </button>
+        </h2>
+      </div>
+      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+        <div class="card-body">
+          Podes contactarnos a través del <a href="contactanos.php"> Formulario de contacto </a> o enviando tu inquietudes a sharityx@gmail.com
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header" id="headingThree">
+        <h2 class="mb-0">
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            ¿Cómo hago para participar?
+          </button>
+        </h2>
+      </div>
+      <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+        <div class="card-body">
+          Para poder participar debes estar registrado, ya seas una persona o una organización.
+        </div>
+      </div>
+    </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          ¿Tiene algún costo?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+        No! Desde Sharityx deseamos que seamos muchos buscando transformar la realidad, es por ellos que este espacio será siempre gratuito.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          ¿Cómo cambio la información de mi perfil?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+        Podes cambiar la información de tu perfil a través de la pestaña "Editar" en la solapa <a href="perfil.php">Perfil </a>.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h2 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          ¿Cómo cambio mi contraseña?
+        </button>
+      </h2>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+        Podes cambiar tu contraseña a través de la pestaña "Cambiar contraseña" en la solapa "Seguridad".
+      </div>
+    </div>
+  </div>
+  </div>
+    </main>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
