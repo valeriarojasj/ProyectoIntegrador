82f3c271881dcfe01d46ea3f41f66c41c15ddd3f
LautaroValle <lautarovallelopez@gmail.com>
2019-09-27 23:38:00 -0300

    agregando footer

--- home.php
+++ home.php
@@ -109,7 +109,9 @@ require_once('funciones.php');
 
 </div>
 </main>
-
+<?php
+include_once('footer.php');
+?>
 
 <script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
